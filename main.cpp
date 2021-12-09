075129c70718429a49e7252bf9f9db6eaa271965
alice <alice@fox.com>
2021-12-09 14:26:08 +0800

    Add 'format' option

--- main.cpp
+++ main.cpp
@@ -3,10 +3,31 @@
 #include <iomanip>
 #include <iostream>
 
-int main()
+#include "lib/cxxopts.hpp"
+
+auto make_options()
 {
+    cxxopts::Options options("now", "Print now!");
+    options
+        .set_tab_expansion()
+        .add_options()("f,format", "Specify format", cxxopts::value<std::string>()->default_value("%Y-%m-%d %X"))("h,help", "Print usage");
+    return options;
+}
+
+int main(int argc, char **argv)
+{
+    auto options = make_options();
+    auto result = options.parse(argc, argv);
+
+    if (result.count("help"))
+    {
+        std::cout << options.help() << std::endl;
+        exit(0);
+    }
+
+    auto format = result["format"].as<std::string>().c_str();
     auto now = std::chrono::system_clock::now();
     auto time = std::chrono::system_clock::to_time_t(now);
-    auto formatted = std::put_time(std::localtime(&time), "%Y-%m-%d %X");
+    auto formatted = std::put_time(std::localtime(&time), format);
     std::cout << formatted << std::endl;
 }
